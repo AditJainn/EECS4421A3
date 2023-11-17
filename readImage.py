a124f08aec987bf2b245b942bbf8455d32403562
yates-nikhil <nikhilyy@my.yorku.ca>
2023-11-16 20:36:11 -0500

    Update readImage.py

--- readImage.py
+++ readImage.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import math
 import random
+from scipy.spatial import KDTree
 
-numberOfNodes = 500
+numberOfNodes = 200
 
 class point:
     def __init__(self, x, y,radius = 1):
@@ -21,29 +22,34 @@ def point_obst_overlap(map, p):
         return overlap
 
     mapx, mapy = p.x, p.y
+
     if is_not_free(mapx, mapy):
         return True    
     return False
 
 # USE THIS METHOD TO DETIRMINE IF A LINE BETWEEN TWO VALID POINTS CROSSES OVER AN OBSTACLE
-def line_color_intersection(world, v1, v2):
+def line_color_intersection(map, v1, v2):
     # we are going to check if the pixel at the specified coordinates is white
     def is_not_white(x, y):
-        return not all(world[y, x] == [255, 255, 255])
+        lineOverlap = False
+        if all(map[y, x] == [0, 0, 0]):
+            lineOverlap = True
+            print('LINE COLOR INTERSECTION HAS OCCURED')
+        return lineOverlap 
     # first, we are going to assign the point coordinates to new variables
     x1, y1, x2, y2 = v1.x, v1.y, v2.x, v2.y
     dx, dy = abs(x2 - x1), abs(y2 - y1)
     # now, the x and y coordinates will be that of the first point
     x, y = x1, y1
+    print('X COORD TO BE CHECKED = ',x)
+    print('Y VALUE TO BE CHECKED = ', y)
     # here, we do some shifting by steps. If x1 > x2. For example, if x1 is greater than x2 then we 
     # will take a negative step in the x axis with sx. Similar for the y axis with sy
     sx = -1 if x1 > x2 else 1
     sy = -1 if y1 > y2 else 1
-    # err represents the direction we will move in along the line
+    # err represents the direction we will move in along the line to get from one point to the next
     err = dx - dy
     while True:
-        if is_not_white(x, y):
-            return True
         if x == x2 and y == y2:
             break
         e2 = 2 * err
@@ -53,14 +59,16 @@ def line_color_intersection(world, v1, v2):
         if e2 < dx:
             err += dx
             y += sy
+        if is_not_white(x, y):
+            return True
+    print('NO LINE COLOR INTERSECTION')
     return False
-
 
 #=========================================================================
 
 # first need to define the list of generated points
 
-map = cv2.imread('map.jpg')
+map = cv2.imread('map2.jpg')
 
 randomPoints = [(np.random.randint(10, 990), np.random.randint(10, 990)) for _ in range(numberOfNodes)]
 listOfVertix = []
@@ -80,56 +88,75 @@ def findDistace (p1, p2):
     distance = math.sqrt((p1.x - p2.x)**2 + (p2.y - p1.y)**2)
     return distance
 
-# FIND THE CLOSEST VERTEX TO OUR POINT
-def findClosetVertex(exploredList, newVertex): 
-    closestVertex = 0 
-    distance = -1 
-    for v in exploredList:
-        calculatedDistance = findDistace(v,newVertex) 
-        if distance == -1 or calculatedDistance < distance:
-            closestVertex = v
-    return closestVertex
+# DRAW LINE BETWEEN POINTS
 def drawLine(v1,v2):
-    cv2.line(map, (v1.x,v1.y), (v2.x,v2.y), (0,0,0), thickness=2)
+    cv2.line(map, (v1.x,v1.y), (v2.x,v2.y), (128,0,128), thickness=2)
 
-# FIND THE CLOSEST POINT TO THE ENTIRE GRAPH
-def findClosetNodeToGraph(exploredVertexList,listOfVertix):
-    newConnection = 0 # This will be two different vertexes we will be returning
-    smallestDistance = -1 
+# FIND THE CLOSEST NODE TO A GRAPH
+def findClosestNodeToGraph(exploredVertexList, listOfVertix):
+    # Convert vertex points to a list of tuples
+    unexploredPoints = [(v.x, v.y) for v in listOfVertix]
+
+    # Create a KD-tree with unexplored vertices
+    tree = KDTree(unexploredPoints)
+
+    smallestDistance = float('inf')
+    newConnection = None
+
     for exploredV in exploredVertexList:
-        for unexploredVertix in listOfVertix:
-            calculateDistance = findDistace(exploredV,unexploredVertix)
-            if smallestDistance == -1 or calculateDistance < smallestDistance:
-                smallestDistance = calculateDistance
-                newConnection = (exploredV,unexploredVertix)
-    #return the new "line"
+        # Find the nearest neighbor to exploredV in the KD-tree
+        distance, index = tree.query((exploredV.x, exploredV.y))
+
+        #if line_color_intersection(map, exploredV, listOfVertix[index]):
+
+        if distance < smallestDistance:
+            smallestDistance = distance
+            newConnection = (exploredV, listOfVertix[index])
+            print('NEW CONNECTION MADE')
+        # Early exit if the distance is zero
+        if distance == 0:
+            break
+
     return newConnection
 
-exploredVertexList = []
-startVertex = listOfVertix.pop()
-
-
-cv2.circle(map, (startVertex.x,startVertex.y), 6, (0,255,0), thickness=-1)
-
-exploredVertexList.append(startVertex)
-
-while(len(listOfVertix) > 0 ): 
-    graphNode, newNode = findClosetNodeToGraph(exploredVertexList, listOfVertix)
-    
-
-    if line_color_intersection(map, graphNode, newNode):
-        drawLine(graphNode,newNode) 
-    
-    exploredVertexList.append(newNode)
-    listOfVertix.remove(newNode)
-    
 
 def main():
+    
+    # this is essentially our RRT list of nodes 
+    exploredVertexList = []
+    startVertex = listOfVertix.pop()
+
+    cv2.circle(map, (startVertex.x,startVertex.y), 6, (0,255,0), thickness=-1)
+
+    exploredVertexList.append(startVertex)
+    print('List of vertix: ', len(listOfVertix))
+
+    while(len(listOfVertix) > 0):
+        
+        graphNode, newNode = findClosestNodeToGraph(exploredVertexList, listOfVertix)
+
+
+        if line_color_intersection(map, graphNode, newNode) == False:
+            drawLine(graphNode, newNode) 
+            exploredVertexList.append(newNode)
+            listOfVertix.remove(newNode)
+
+
+            print('ELEMENTS IN LIST OF VERTIX: ', len(listOfVertix))
+            print('GRAPH NODE: ', graphNode.x, graphNode.y)
+            print('NEW NODE: ', newNode.x, newNode.y)
+            print('*******NODE ADDED TO RRT*******')
+        else:
+            # this node has already been explored so we remove it from our list
+            listOfVertix.remove(newNode)
+        print('\n')
+
     cv2.imshow('colour-based', map)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    # plt.figure(figsize=(10, 15))
-    # plt.imshow(white_image)
-    # plt.show()
+    
+    print('List of vertix: ', len(listOfVertix))    
+    for p in exploredVertexList:
+        print(p.x, p.y)
 
 main()
